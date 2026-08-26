c48c915e7d59e3d1929f18ea645dc70c585a0ae1
tangball <tinndo01@stu.jejunu.ac.kr>
2026-08-26 09:45:53 +0900

    2026.8.24 작성

--- 코딩테스트/JAVA/test.java
+++ 코딩테스트/JAVA/test.java
@@ -1,37 +1,61 @@
 import java.util.*;
-import java.util.stream.Stream;
-import java.io.*;
 
 public class test {
     
     public static void main(String[] args) {
-        String inputstr = "1 2 Z 3";
+        String inputstr = "1a2b3c4d123Z";
         int inputnum = 9;
-        int[] inputarr = {1, 2, 3, 4, 5};
+        int[][] inputarr = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
         test myTest = new test();
 
-        int result = myTest.solution(inputstr);
+        int result = myTest.solution(inputarr);
         System.out.println("result: " + result);
     }
 
 
-    public int solution(String s) {
-        /*일단 할 일 : 공백 기준 split
-        그러면 1, 2, Z, 3이 나와.
-        그 중 Z의 인덱스를 찾아야 해.
-        그러면 역순으로. 진행.*/
-        String[] str = s.split(" ");
+
+    public int solution(int[][] board) {
         int answer = 0;
-        for(int i=0;i>str.length;i++){
-            if(str[i].equals("Z")){
-                answer -= Integer.parseInt(str[i-1]);
-            }else{
-                answer += Integer.parseInt(str[i]);
+        System.out.println("board: " + Arrays.deepToString(board));
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]==1){
+                    for(int x=Math.max(i-1,0);x<Math.min(board.length, i+2);x++){
+                        for(int y=Math.max(j-1,0);y<Math.min(board[0].length, j+2);y++){
+                            board[x][y] += 1;
+                        }
+                    }
+                }
             }
-            
+        }
+        System.out.println("board: " + Arrays.deepToString(board));
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]==0){
+                    answer ++;
+                }
+            }
         }
         return answer;
     }
 
-
 }
+
+1 1
+진입
+2 2
+3 3
+3 4
+4 5
+5 6
+5 7
+6 8
+7 9
+7 10
+8 11
+9 12
+9 13
+9 14
+10 15
+
+
